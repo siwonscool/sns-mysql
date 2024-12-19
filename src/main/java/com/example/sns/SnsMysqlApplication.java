2416f0d7b9d44b0817a53d79c933bb24593a97c4
kevin-0213 <kevin.0213@kakaocorp.com>
2024-12-19 19:41:14 +0900

    Initial commit

---
+++ src/main/java/com/example/sns/SnsMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsMysqlApplication.class, args);
+    }
+
+}
